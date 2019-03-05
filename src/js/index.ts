e239a1562979317b1355b2aa94a224f731c4b84f
basicxd <mikail-fener@hotmail.com>
2019-03-05 11:43:51 +0100

    git commit - m "For Loop"

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,32 @@
-//Her skal du skrive din typescriptkode
+
+
+// let gettingID = document.getElementById('0');
+
+// console.log(gettingID)
+
+
+// let gettingClasseName = document.getElementsByClassName('usundt');
+
+// console.log(gettingClasseName)
+
+// let gettingTagName = document.getElementsByTagName('li');
+
+// console.log(gettingTagName)
+
+
+// let gettingListe = document.getElementById('liste');
+
+// console.log(gettingListe)
+
+let loopFunction = document.getElementById('liste');
+function run(){
+    let gettingIdLI = document.getElementsByTagName('li');
+    for(let i = 0; i < gettingIdLI.length; i++)
+    {
+        console.log('I:'+ i)
+        console.log(gettingIdLI[i])
+    }
+}
+run()
+ 
+
