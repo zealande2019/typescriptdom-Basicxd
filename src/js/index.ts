db2681496ed12ed77d90ea39cb2bc7dbe8fbf345
Mikail Fener <mika1292@edu.easj.dk>
2019-03-06 15:03:06 +0100

    Opg 2-3 Done

--- src/js/index.ts
+++ src/js/index.ts
@@ -18,15 +18,50 @@
 
 // console.log(gettingListe)
 
+console.log('Opgave 2')
+
 let loopFunction = document.getElementById('liste');
 function run(){
     let gettingIdLI = document.getElementsByTagName('li');
     for(let i = 0; i < gettingIdLI.length; i++)
     {
-        console.log('I:'+ i)
+
         console.log(gettingIdLI[i])
     }
 }
 run()
- 
+
+console.log('Opgave 3:')
+
+// Punkt 1
+// let ChangeAtbEleRemove = document.getElementById("1").removeAttribute("sundt");
+// let ChangeAtbEleSet = document.getElementById("1").setAttribute("class", "usundt");
+
+// Punkt 2
+// let HideEm = document.getElementById("3").hidden = true;
+
+// let HideEm = document.getElementById("3").setAttribute("hidden", "true");
+
+// Punkt 3
+// function myFunction() 
+// {
+//     let gettingIdLI = document.getElementsByTagName('li');
+//     for(let i = 0; i < gettingIdLI.length; i++)
+//     {
+//     document.getElementById(i).classList.remove("usundt");
+//     document.getElementById(i).classList.add("sundt")
+//     }
+// }
+// myFunction()
+
+// function run2(){
+//     let gettingIdLI = document.getElementsByTagName('li');
+//     for(let i = 0; i < gettingIdLI.length; i++)
+//     {
+//         console.log(gettingIdLI[i])
+//     }
+// }
+// run2()
+
+console.log('Opgave 4:')
 
